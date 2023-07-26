fae264eb3e6b31e6ad3ce6d6b1465c46d53f9d4b
Kaik <bakukai@gmail.com>
2023-07-25 21:13:20 -0400

    new

--- components/Descriptions/Description.js
+++ components/Descriptions/Description.js
@@ -9,7 +9,7 @@ const Description = () => {
       <p>Motivational interviewing is a counseling approach that helps people find the motivation to make positive behavior changes. It is particularly effective for people who have mixed feelings about changing their behavior. This client-centered approach empowers individuals to take responsibility for their own recovery by resolving ambivalence and increasing motivation to change.</p>
       <p>This is an experiment in developing more acccesible possibilities for client center coaching practices. We do not collect any data, however, responses are sent through an API controlled by OpenAI. Please do not share anything confidential. Also, LLMs such as ChatGPT can be bias, inappropriate and offer unsafe advice. Pleaase only use this app for experimentation/research purposes.</p>
 
-      <a href="https://www.wikipedia.org/">More about Motivational Interviewing</a>
+      <a href="https://en.wikipedia.org/wiki/Motivational_interviewing">More about Motivational Interviewing</a>
     </div>
   );
 }
